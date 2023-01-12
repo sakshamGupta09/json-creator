9bfbdea1a10b439ba7d52f39e25d4fc1296f5e20
Saksham Gupta <saksham.09.gupta@gmail.com>
2023-01-12 09:52:36 +0530

    retreive and render json

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,17 @@
 
 const parentJsonElement = document.getElementById("parent-container");
 
+const jsonContainerElement = document.querySelector(".json__container");
+
+const getJsonBtnElement = document.getElementById("get-json-btn");
+
+const displayJsonValueElement = document.getElementById("json-value");
+
 // Event handlers
 
 parentJsonElement.addEventListener("click", parentClickHandler);
+
+getJsonBtnElement.addEventListener("click", getJSONClickHandler);
 
 // Functions
 
@@ -24,6 +32,11 @@ function parentClickHandler(e) {
     collapseRow(e);
     return;
   }
+}
+
+function getJSONClickHandler() {
+  const json = getJson();
+  renderJSONToUi(json);
 }
 
 // Utilities
@@ -57,4 +70,43 @@ function getJsonRowElement() {
 
 function collapseRow(e) {
   e.target.classList.toggle("json__caret--close");
+}
+
+function getJson(node = jsonContainerElement) {
+  const object = {};
+
+  const keyInputElement = node.querySelector('.json__input[name="key"]');
+  const valueInputElement = node.querySelector('.json__input[name="value"]');
+
+  const children = getChildrenWithClassName(node, ".json__sub-item");
+
+  if (!children.length) {
+    object[keyInputElement.value] = valueInputElement.value;
+    return object;
+  }
+  let obj = {};
+
+  for (let i = 0; i < children.length; i++) {
+    obj = { ...obj, ...getJson(children[i]) };
+  }
+  object[keyInputElement.value] = obj;
+  return object;
+}
+
+function getChildrenWithClassName(node, className) {
+  let child = node.querySelector(className);
+
+  if (!child) return [];
+
+  const children = [child];
+  while (child.nextElementSibling) {
+    children.push(child.nextElementSibling);
+    child = child.nextElementSibling;
+  }
+  return children;
+}
+
+function renderJSONToUi(json) {
+  json = JSON.stringify(json);
+  displayJsonValueElement.textContent = json;
 }
